946971e4ef352977c6e655f4fb31bf4211d367fe
raneemts <raneem_alsaqat@hotmail.com>
2026-04-15 12:24:43 +0000

    tt

--- TypeScript/task_1/js/main.js
+++ TypeScript/task_1/js/main.js
@@ -19,6 +19,33 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName[0]}. ${lastName}`;
 };
 
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -37,4 +64,8 @@ const director1: Directors = {
 
 console.log(teacher3);
 console.log(director1);
-console.log(printTeacher("John", "Doe"));
+console.log(printTeacher('John', 'Doe'));
+
+const student = new StudentClass('John', 'Doe');
+console.log(student.displayName());
+console.log(student.workOnHomework());
